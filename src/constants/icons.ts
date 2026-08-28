a52d1094c0b16fc8d7f6d502e0d4aafac315e95c
nevena <dnevena27@gmail.com>
2026-08-28 17:13:13 +0300

    chore(assets): add SVG icon set and icons constants
    Add 13 SVG icons (home, search, bookmark, profile, play, plus, upload, menu, logout, eye/eye-hide, leftArrow/rightArrow) and src/constants/icons.ts to expose them.

---
+++ src/constants/icons.ts
@@ -0,0 +1,29 @@
+import bookmark from "../../assets/icons/bookmark.svg";
+import home from "../../assets/icons/home.svg";
+import plus from "../../assets/icons/plus.svg";
+import profile from "../../assets/icons/profile.svg";
+import leftArrow from "../../assets/icons/leftArrow.svg";
+import menu from "../../assets/icons/menu.svg";
+import search from "../../assets/icons/search.svg";
+import upload from "../../assets/icons/upload.svg";
+import rightArrow from "../../assets/icons/rightArrow.svg";
+import logout from "../../assets/icons/logout.svg";
+import eyeHide from "../../assets/icons/eye-hide.svg";
+import eye from "../../assets/icons/eye.svg";
+import play from "../../assets/icons/play.svg";
+
+export default {
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
