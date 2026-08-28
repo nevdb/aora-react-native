a60ca6614dc746d07ab1f4f546c2baf731c4db55
nevena <dnevena27@gmail.com>
2026-08-28 10:44:18 +0300

    initial setings and adding nativewind

---
+++ src/app/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
